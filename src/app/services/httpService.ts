ad52ff365dcfda28cd6cc454376fbd033a399d33
Sandeepk <sandeepk@IN-sandeepk.Dlink>
2017-02-03 22:45:56 +0530

    [Sandeep] added get call builder to httpService

--- src/app/services/httpService.ts
+++ src/app/services/httpService.ts
@@ -7,8 +7,20 @@ export class HttpService {
 
   constructor(private http: Http) { }
 
+  private encodeQueryData(params: {}): string {
+    let ret: any[];
+    for (let param in params)
+      ret.push(encodeURIComponent(param) + '=' + encodeURIComponent(params[param]));
+    return ret.join('&');
+  }
+
+
   post(relativeUrl: string, params: {}){
     return this.http.post(this.baseUrl + relativeUrl, params)
+  }
+
+  get(relativeUrl: string, params: {}) {
+    return this.http.get(this.baseUrl + relativeUrl + "?" + this.encodeQueryData(params))
   }
 
 }
