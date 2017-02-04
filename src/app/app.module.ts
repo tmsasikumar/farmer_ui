eef6fbcfff7365bf514bd7f9c9a042e2a4b6e05a
sandeeph <sandeeph@thoughtworks.com>
2017-02-04 21:08:47 +0530

    Home page with basic Home page structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,12 @@ import {HttpService} from "./services/httpService";
 import { Routing } from "./app.routing";
 import {AuthGuard} from "./guards/auth.guard";
 import { CommonModule } from '@angular/common';
-import {HeaderComponent} from "./components/header/header.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
   imports:      [ BrowserModule, Routing, FormsModule, HttpModule, CommonModule ],
-  declarations: [ AppComponent, HomeComponent, LoginComponent, RegisterComponent, DashboardComponent, HeaderComponent],
+  declarations: [ AppComponent, HomeComponent, LoginComponent, RegisterComponent, DashboardComponent, HeaderComponent, FooterComponent],
   providers: [ AuthenticationService, HttpService , AuthGuard],
   bootstrap:    [ AppComponent ]
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,7 +4,7 @@ import {Router} from "@angular/router";
 
 @Component({
   moduleId: module.id,
-  selector: 'header',
+  selector: 'app-header',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent  {
