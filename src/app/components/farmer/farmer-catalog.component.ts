eb529341df0e432559afd97bda1211aa82dd411f
Sandeepk <sandeepk@IN-sandeepk.local>
2017-02-05 03:39:24 +0530

    [Sandeep/Prakash] Added search for farmer by location

--- src/app/components/farmer/farmer-catalog.component.ts
+++ src/app/components/farmer/farmer-catalog.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 import {farmerService} from "../../services/farmer-service";
+import {Crops} from "../../enums/crops.enum";
 
 
 @Component({
@@ -11,13 +12,30 @@ import {farmerService} from "../../services/farmer-service";
 
 export class FarmerCatalogComponent implements OnInit {
   private farmers: Farmer[];
+  private filteredFarmer: Farmer[];
+  crops: any;
 
   constructor(private farmerService: farmerService) {
+    this.crops = Crops;
   }
 
   ngOnInit() {
     this.farmerService.getAllFarmers()
-      .subscribe(AllFarmers => this.farmers = AllFarmers);
+      .subscribe(AllFarmers => {
+        this.farmers = AllFarmers;
+        this.filteredFarmer = this.farmers;
+      });
+
+  }
+  
+  search(location: string){
+    this.filteredFarmer = this.farmers.filter(this.filterByString(location));
+  }
+  private filterByString(filter: string) {
+    filter = filter.toLowerCase();
+    return (value: {}) => {
+      return !filter || value["landAddress"].toLowerCase().indexOf(filter) !== -1;
+    }
 
   }
 }
