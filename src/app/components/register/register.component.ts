95ecab48d368cebebacbb3bd9595615a07bd06e8
Sandeepk <sandeepk@IN-sandeepk.local>
2017-02-04 23:58:16 +0530

    [Sandeep] Added registration page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {AuthenticationService} from "../../services/AuthenticationService";
+import {Router} from "@angular/router";
+import {HttpService} from "../../services/httpService";
 
 @Component({
   moduleId: module.id,
@@ -9,9 +12,25 @@ import {Component} from '@angular/core';
 export class RegisterComponent {
 
   model: any = {};
-  constructor(){
+  constructor(
+    private router: Router,
+    private httpService: HttpService,
+    private authenticationService: AuthenticationService){
   }
 
   register(){
+    this.httpService.post("register", this.model)
+      .subscribe(
+        data => {
+          this.authenticationService.login(this.model.emailId, this.model.password)
+            .subscribe(
+              data => {
+                this.router.navigate(['/dashboard']);
+              },
+              error => {
+              });
+        },
+        error => {
+        });
   }
 }
