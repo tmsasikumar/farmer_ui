d9afb5b256ff5aa361bb59077d61da7ebbc1497e
sandeeph <sandeeph@thoughtworks.com>
2017-02-06 11:29:39 +0530

    [SandeepH] Added angular cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { SaveTheFarmerPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('save-the-farmer App', function() {
+  let page: SaveTheFarmerPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new SaveTheFarmerPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
